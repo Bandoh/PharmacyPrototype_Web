c1859fab6e4dfe4d6edfb13c4039aad6ce630fd2
Bandoh <kelvinbandoh@hotmail.com>
2018-06-15 20:04:00 +0000

    valid

--- customJs/login.js
+++ customJs/login.js
@@ -1,19 +1,21 @@
-var attempt=4; 
+var attempt = 4;
 
 
 
-function validate(){ 
-var username= document.getElementById("exampleInputEmail1").value; 
-var password= document.getElementById("exampleInputPassword1").value; 
+function validate() {
+    var username = document.getElementById("exampleInputEmail1").value;
+    var password = document.getElementById("exampleInputPassword1").value;
 
-if((username=="admin@admin")&&(password=="root")){ 
- 
-alert("you've successfully loged in");
-return true;
-} 
-else{  
-    alert("wrong username or password");
-    return false;
-}
+    if ((username == "admin@admin") && (password == "root")) {
+
+        alert("you've successfully loged in");
+        return true;
+    }
+    else {
+        attempt--;
+        alert("wrong username or password");
+        console.log(attempt);
+        return false;
+    }
 
 }
