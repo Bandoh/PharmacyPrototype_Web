c73218db14fe91d34143da2a61e1e63ed1c047c7
Bandoh <kelvinbandoh@hotmail.com>
2018-06-23 18:25:50 +0000

    sidebar

---
+++ js/customJs/home.js
@@ -0,0 +1,44 @@
+function contactPage() {
+    var contactpage = document.getElementById('contact');
+    var settingspage = document.getElementById('settings');
+    var stockpage = document.getElementById('stock');
+    var pagetextcontainer = document.getElementById('pageContainer');
+    contactpage.style.display = 'block';
+    stockpage.style.display = 'none';
+    settingspage.style.display = 'none';
+    pagetextcontainer.style.display = 'none';
+}
+
+function stockPage() {
+    var contactpage = document.getElementById('contact');
+    var settingspage = document.getElementById('settings');
+    var stockpage = document.getElementById('stock');
+    var pagetextcontainer = document.getElementById('pageContainer');
+    contactpage.style.display = 'none';
+    stockpage.style.display = 'block';
+    settingspage.style.display = 'none';
+    pagetextcontainer.style.display = 'none';
+}
+
+function settingsPage() {
+    var contactpage = document.getElementById('contact');
+    var settingspage = document.getElementById('settings');
+    var stockpage = document.getElementById('stock');
+    var pagetextcontainer = document.getElementById('pageContainer');
+    contactpage.style.display = 'none';
+    stockpage.style.display = 'none';
+    settingspage.style.display = 'block';
+    pagetextcontainer.style.display = 'none';
+}
+
+
+function pageContainerPage() {
+    var contactpage = document.getElementById('contact');
+    var settingspage = document.getElementById('settings');
+    var stockpage = document.getElementById('stock');
+    var pagetextcontainer = document.getElementById('pageContainer');
+    contactpage.style.display = 'none';
+    stockpage.style.display = 'none';
+    settingspage.style.display = 'none';
+    pagetextcontainer.style.display = 'block';
+}
