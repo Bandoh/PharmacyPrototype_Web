07ca23efdfd35e40b29a139c6b51ddaaa82d0482
RBayor <rbayor16@gmail.com>
2018-06-24 15:41:41 +0000

    update to overview

--- js/firebaseJs/myPharmacy.js
+++ js/firebaseJs/myPharmacy.js
@@ -6,7 +6,7 @@ db.collection("PharmacyDetails").where("E-mail", "==", 'something@gh.com')
         querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
             
-            overViewList.innerHTML += "<div id='StockItems'><h4>"+ doc.data().Stock.StockName + "</h4><h4>"+ doc.data().Stock.StockPrice +"</h4><h4>"+ doc.data().Stock.Available +"</h4></div>"
+            overViewList.innerHTML += "<div id='StockItems'><h4>"+ doc.data().Stock.StockName + "</h4><h4>"+ doc.data().Stock.StockPrice +"</h4><h4>"+ doc.data().Stock.Available +"</div>"
 
         });
     })
